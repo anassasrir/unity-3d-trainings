0b70068d07f121bc12946254373c5b75a771b444
anassasrir <asrir.anass@hotmail.fr>
2025-11-25 18:53:05 +0100

    refactor previous code into methods

--- ObstacleDodge/Assets/Scripts/Mover.cs
+++ ObstacleDodge/Assets/Scripts/Mover.cs
@@ -8,16 +8,26 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoveDodger();
+    }
+
+    void PrintInstructions() 
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move the Dodger around the level, and avoid bumping into obstacles!!");
+    }
+
+    void MoveDodger()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
-        
     }
 }
